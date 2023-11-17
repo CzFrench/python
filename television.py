688be9aca20abe8684eaf3c82603c136a0ccd420
CzFrench <151091587+CzFrench@users.noreply.github.com>
2023-11-17 14:26:41 -0600

    Television remote logic

---
+++ television.py
@@ -0,0 +1,52 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = 0
+        self.__channel = 0
+        self.__volume_before_mute = 0
+
+    def power(self):
+        self.__status = not self.__status
+
+    def mute(self):
+        if self.__status:
+            self.__muted = not self.__muted
+            if self.__muted:
+                self.__volume_before_mute = self.__volume
+                self.__volume = Television.MIN_VOLUME
+            else:
+                self.__volume = self.__volume_before_mute
+
+    def channel_up(self):
+        if self.__status:
+            self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
+
+    def channel_down(self):
+        if self.__status:
+            self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
+
+    def volume_up(self):
+        if self.__status:
+            if self.__muted:
+                self.mute()
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self):
+        if self.__status:
+            if self.__muted:
+                self.mute()
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+
+    def __str__(self):
+        if self.__muted:
+            return f'Power = {self.__status}, Channel = {self.__channel} Volume = {Television.MIN_VOLUME}'
+        else:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
